99d3b6a510d510c10e90181600871fd646642f07
Fernando Agüero <fjaguero@gmail.com>
2016-01-21 00:50:15 +0100

    Add home and MGT buttons on MainHeader

--- app/client/components/layouts/main/MainHeader.jsx
+++ app/client/components/layouts/main/MainHeader.jsx
@@ -1,31 +1,45 @@
 Camino.MainHeader = React.createClass({
     mixins: [ReactMeteorData],
     getMeteorData() {
-        return {
-            currentUser: Meteor.user()
-        };
+      return {
+          currentUser: Meteor.user()
+      };
     },
     handleLogout() {
-        Meteor.logout(() => {
-          FlowRouter.go('Login');
-        });
+      Meteor.logout(() => {
+        FlowRouter.go('Login');
+      });
+    },
+    navigateTo(route) {
+      FlowRouter.go(route)
     },
     render() {
-        let logoutButton;
-        let { currentUser } = this.data;
+      let logoutButton
+      let { currentUser } = this.data
+      let mitButton
 
-        if (currentUser) {
-            logoutButton = (
-              <li><a onClick={this.handleLogout}>Logout</a></li>
-            );
-        }
+      homeButton = (
+        <li><a onClick={this.navigateTo.bind(this, '/')}>Home</a></li>
+      )
 
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container">
-                  {logoutButton}
-                </div>
-            </nav>
-        );
+      mgtButton = (
+        <li><a onClick={this.navigateTo.bind(this, '/morning-gratitude')}>Morning Gratitude</a></li>
+      )
+
+      if (currentUser) {
+          logoutButton = (
+            <li><a onClick={this.handleLogout}>Logout</a></li>
+          );
+      }
+
+      return (
+          <nav className="navbar navbar-default">
+              <div className="container">
+                {homeButton}
+                {mgtButton}
+                {logoutButton}
+              </div>
+          </nav>
+      );
     }
 });
