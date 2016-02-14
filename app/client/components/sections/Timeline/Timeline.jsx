ac04fa3949a2281240bff2e6a15b7d1454028720
Fernando Agüero <fjaguero@gmail.com>
2016-02-14 20:46:35 +0100

    Add TimelineTable and user Timeline as a Wrapper

--- app/client/components/sections/Timeline/Timeline.jsx
+++ app/client/components/sections/Timeline/Timeline.jsx
@@ -1,91 +1,31 @@
 Camino.Timeline = React.createClass({
-  // mixins: [ReactMeteorData],
+  mixins: [ReactMeteorData],
 
-  // getMeteorData() {
-  // 
-  // },
+  getMeteorData() {
+    let sub = Meteor.subscribe("tasks");
 
-  getInitialState: function () {
-		return {
-			'selectedItems': [],
-			tolerance: 0,
-			distance: 1,
-			isGlobal: true
-		};
-	},
+    // Get the tasks for the current day
+    let today = moment().startOf('day')
+    let tomorrow = moment(today).add(1, 'days')
 
-  handleSelection: function(keys) {
-    alert(keys)
+    let tasks = Tasks.find({
+      'userId': Meteor.userId(),
+      'createdAt': {
+        $gte: today.toDate(),
+        $lt: tomorrow.toDate()
+      }
+    }).fetch();
+
+    console.log('TASKS =>')
+    console.log(tasks)
+
+    return {'tasks': tasks}
   },
 
   render() {
-    data = [
-      { hour: '8'},
-      { hour: '8:30'},
-      { hour: '9'},
-      { hour: '9:30'},
-      { hour: '10'},
-      { hour: '10:30'},
-      { hour: '11'},
-      { hour: '11:30'},
-      { hour: '12'},
-      { hour: '12:30'},
-      { hour: '13'},
-      { hour: '13:30'},
-      { hour: '14'},
-      { hour: '14:30'},
-      { hour: '15'},
-      { hour: '15:30'},
-      { hour: '16'},
-      { hour: '16:30'},
-      { hour: '17'},
-      { hour: '17:30'},
-      { hour: '18'},
-      { hour: '18:30'},
-      { hour: '19'},
-      { hour: '19:30'},
-      { hour: '20'},
-      { hour: '20:30'},
-      { hour: '21'},
-      { hour: '21:30'},
-      { hour: '22'},
-      { hour: '22:30'},
-      { hour: '23'},
-      { hour: '23:30'},
-      { hour: '24'}
-    ]
-
     return (
-      <div className="timeline mit__container">
-        <h2 className="title">Timeline</h2>
-          <Selectable
-  					className="main"
-  					onSelection={this.handleSelection}
-  					tolerance={this.state.tolerance}
-  					globalMouse={this.state.isGlobal}
-  					distance={this.state.distance}
-            selectboxBorderColor="#000">
-
-    				{data.map(function (item, i) {
-    					return <Item key={item.hour} hour={item.hour} />
-    				})}
-  				</Selectable>
-
-      </div>
-    );
+      <TimelineTable tasks={this.data.tasks || []} />
+    )
   }
 
-});
-
-
-var Item = React.createClass({
-	render: function () {
-		var classes = this.props.selected ? 'item selected' : 'item';
-		return (
-			<div className={classes}>
-				<span className="item-hour">{this.props.hour}</span>
-			</div>
-		);
-
-	}
 });
---
+++ app/client/components/sections/Timeline/TimelineTable.jsx
@@ -0,0 +1,139 @@
+TimelineTable = React.createClass({
+
+  getInitialState() {
+		return {
+			'selectedItems': []
+		}
+	},
+
+  handleOnDeleteTask(taskId) {
+    Tasks.remove({
+      '_id': taskId
+    })
+  },
+
+  handleSelection: function(timeSelection) {
+    // Check if we already have a task for that time span
+    console.log('Time selection =>')
+    console.log(timeSelection)
+
+    let taskName = window.prompt('Task Name')
+
+    let task = {
+      'name': taskName,
+      'range': timeSelection
+    }
+
+    // Create the task
+    if (taskName) {
+      Meteor.call('createTask', task)
+    }
+  },
+
+  render() {
+    timeRanges = [
+      { hour: '8:00'},
+      { hour: '8:30'},
+      { hour: '9:00'},
+      { hour: '9:30'},
+      { hour: '10:00'},
+      { hour: '10:30'},
+      { hour: '11:00'},
+      { hour: '11:30'},
+      { hour: '12:00'},
+      { hour: '12:30'},
+      { hour: '13:00'},
+      { hour: '13:30'},
+      { hour: '14:00'},
+      { hour: '14:30'},
+      { hour: '15:00'},
+      { hour: '15:30'},
+      { hour: '16:00'},
+      { hour: '16:30'},
+      { hour: '17:00'},
+      { hour: '17:30'},
+      { hour: '18:00'},
+      { hour: '18:30'},
+      { hour: '19:00'},
+      { hour: '19:30'},
+      { hour: '20:00'},
+      { hour: '20:30'},
+      { hour: '21:00'},
+      { hour: '21:30'},
+      { hour: '22:00'},
+      { hour: '22:30'},
+      { hour: '23:00'},
+      { hour: '23:30'},
+      { hour: '24:00'}
+    ]
+
+    return (
+      <div className="timeline mit__container">
+        <h2 className="title">Timeline</h2>
+
+          <Selectable
+  					className="main"
+  					onSelection={this.handleSelection}
+            selectboxBorderColor="#000">
+    				{
+              timeRanges.map((item, i) => {
+                return <TimeRange
+                  onClick={this.handleClick}
+                  key={item.hour}
+                  hour={item.hour}
+                  tasks={this.props.tasks}
+                  onDeleteTask={this.handleOnDeleteTask}
+                />
+    				  })
+            }
+  				</Selectable>
+
+      </div>
+    );
+  }
+
+});
+
+
+let TimeRange = React.createClass({
+
+  handleClick(e) {
+    // Allow deletion of a single task
+    if (this.task && this.task.name) {
+      let confirmDelete = window.confirm("Delete the task?")
+
+      if (confirmDelete) {
+        this.props.onDeleteTask(this.task._id)
+        this.task = {}
+      }
+    }
+
+  },
+
+	render() {
+		let classes = this.props.selected ? 'item selected' : 'item'
+    let taskName = ''
+
+    // Check if there is a task for this time span
+    this.props.tasks.map( (task) => {
+      let hasTask = _.contains(task.range, this.props.hour)
+
+      if (hasTask) {
+        this.task = task
+      }
+      
+    })
+
+    if (this.task && this.task.name) {
+      taskName = this.task.name
+    }
+
+		return (
+			<div onClick={this.handleClick} className={classes}>
+				<span className="item-hour">{this.props.hour}</span>
+        <span className="item-content">{taskName}</span>
+			</div>
+		);
+
+	}
+});
