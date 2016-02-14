b39a8f17d963ad9ae7c7228f9c1be07ecad1017f
Fernando Agüero <fjaguero@gmail.com>
2016-02-15 00:10:34 +0100

    Add CRUD methods

--- app/server/methods.js
+++ app/server/methods.js
@@ -1,9 +1,42 @@
 Meteor.methods({
+
+  // Tasks
   createTask: function(task) {
     // FIXME: Use collection hooks
     task.createdAt = new Date()
     task.userId = Meteor.userId()
 
     Tasks.insert(task)
+  },
+
+  // Morning Gratitude
+  createGratitude: function(gratitude) {
+    // FIXME: Use collection hooks
+    gratitude.createdAt = new Date()
+    gratitude.userId = Meteor.userId()
+
+    Gratitude.insert(gratitude)
+  },
+
+  updateGratitude: function(id, newValue) {
+    Gratitude.update(id, {
+      $set: {"value": newValue}
+    })
+  },
+
+  // MITs
+  createMits: function(mits) {
+    // FIXME: Use collection hooks
+    mits.createdAt = new Date()
+    mits.userId = Meteor.userId()
+
+    Mits.insert(mits)
+  },
+
+  updateMits: function(id, newValue) {
+    Mits.update(id, {
+      $set: {"value": newValue}
+    })
   }
+
 });
