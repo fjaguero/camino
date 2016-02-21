06433276b5709175a0443cf1a20ffb3bc0502a4e
Fernando Agüero <fjaguero@gmail.com>
2016-02-21 18:15:29 +0100

    Remove boostraped initial user

--- app/server/bootstrap.jsx
+++ app/server/bootstrap.jsx
@@ -2,14 +2,3 @@
 Accounts.config({
   forbidClientAccountCreation: true
 });
-
-Meteor.startup(() => {
-
-    if (Meteor.users.find().count() === 0) {
-        Accounts.createUser({
-            email: "dev@usecamino.com",
-            password: "sherpa"
-        });
-    }
-
-});
