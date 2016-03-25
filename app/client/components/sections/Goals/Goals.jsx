0c617bf3f4cdb841ace412bde82c0f830f3d98b0
Fernando Agüero <fjaguero@gmail.com>
2016-03-25 19:39:19 +0100

    Show <Spinner /> when the subscription is not ready

--- app/client/components/sections/Goals/Goals.jsx
+++ app/client/components/sections/Goals/Goals.jsx
@@ -2,23 +2,36 @@ Camino.Goals = React.createClass({
   mixins: [ReactMeteorData],
 
   getMeteorData() {
-    Meteor.subscribe("goals");
+    let sub = Meteor.subscribe("goals")
 
     return {
+      isLoading: ! sub.ready(),
       goals: Goals.find({
         'userId': Meteor.userId()
       }).fetch()
     }
   },
 
+  showContent() {
+    return (
+      <div className="goals section__container">
+        <GoalsList goals={this.data.goals} />
+      </div>
+    )
+  },
+
+  showSpinner() {
+    return <Spinner />
+  },
+
   render() {
     return (
       <div className="jumbotron">
-          <div className="container">
-            <div className="goals section__container">
-              <GoalsList goals={this.data.goals} />
-          </div>
-          </div>
+        <div className="container">
+          {
+            this.data.isLoading ? this.showSpinner() : this.showContent()
+          }
+        </div>
       </div>
     )
   }
