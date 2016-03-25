42ec68a3b191294bb43d4f0b99e08c9e7a581129
Fernando Agüero <fjaguero@gmail.com>
2016-03-25 19:38:38 +0100

    Add <Spinner />

---
+++ app/client/components/general/Spinner.jsx
@@ -0,0 +1,52 @@
+Spinner = React.createClass({
+  render() {
+    return (
+	    <div className="Spinner">
+	      <div className="Spinner-line Spinner-line--1">
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--left"></div>
+	          </div>
+	          <div className="Spinner-line-ticker">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--center"></div>
+	          </div>
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--right"></div>
+	          </div>
+	      </div>
+	      <div className="Spinner-line Spinner-line--2">
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--left"></div>
+	          </div>
+	          <div className="Spinner-line-ticker">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--center"></div>
+	          </div>
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--right"></div>
+	          </div>
+	      </div>
+	      <div className="Spinner-line Spinner-line--3">
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--left"></div>
+	          </div>
+	          <div className="Spinner-line-ticker">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--center"></div>
+	          </div>
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--right"></div>
+	          </div>
+	      </div>
+	      <div className="Spinner-line Spinner-line--4">
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--left"></div>
+	          </div>
+	          <div className="Spinner-line-ticker">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--center"></div>
+	          </div>
+	          <div className="Spinner-line-cog">
+	              <div className="Spinner-line-cog-inner Spinner-line-cog-inner--right"></div>
+	          </div>
+	      </div>
+	    </div>
+    );
+  }
+});
